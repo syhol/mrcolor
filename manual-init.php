3b4e92de6d9adb3d52cfac1bab773b1d9a06fd05
Simon Holloway <holloway.sy@gmail.com>
2013-05-29 22:53:52 +0100

    Git ignore to ignore sass cache + manual-init.php updates

--- manual-init.php
+++ manual-init.php
@@ -1,11 +1,12 @@
 <?php
 
-$mc = __DIR__ . '/src/SyHolloway/MrColor/';
-$ex = $mc . 'Extension/';
+// get file directory locations
+$ex = ($mc = __DIR__ . '/src/SyHolloway/MrColor/') . 'Extension/' ;
 
-include ($mc . 'Color.php');
-include ($mc . 'Extension.php');
-include ($mc . 'ExtensionCollection.php');
-include ($ex . 'Test.php');
-include ($ex . 'Toolkit.php');
-include ($ex . 'Formatter.php');
+// include all classes
+include($mc . 'Color.php');
+include($mc . 'Extension.php');
+include($mc . 'ExtensionCollection.php');
+include($ex . 'Test.php');
+include($ex . 'Toolkit.php');
+include($ex . 'Formatter.php');
