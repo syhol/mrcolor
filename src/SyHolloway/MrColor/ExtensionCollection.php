9810fb66c5528a265426e46d47d37635efe58796
Simon Holloway <holloway.sy@gmail.com>
2013-09-03 22:40:04 +0100

    PSR-2'd it up

--- src/SyHolloway/MrColor/ExtensionCollection.php
+++ src/SyHolloway/MrColor/ExtensionCollection.php
@@ -15,78 +15,84 @@ use SyHolloway\MrColor\Extension\Formatter;
  */
 class ExtensionCollection
 {
-    
+    /**
+     * Hold an array of Extension objects
+     * @var array
+     */
     private $extensions = array();
-    
+
+    /**
+     * Hold an array of default Extension objects
+     * @var array
+     */
     private static $defaults = array();
-    
+
+    /**
+     * Is the ExtensionCollection initialized yet?
+     * @var boolean
+     */
     private static $init = false;
-    
+
     public function __construct()
     {
-        if( ! self::$init )
-        {
+        if (! self::$init) {
             self::init();
         }
-        
+
         $this->extensions = self::$defaults;
     }
-    
+
     public function runAll(Color $color, $method, $args)
     {
         $result = null;
-        
-        foreach($this->extensions as $extension)
-        {
-            if(true === $extension->query($method))
-            {
+
+        foreach ($this->extensions as $extension) {
+
+            if (true === $extension->query($method)) {
+
                 $result = $extension->trigger($color, $method, $args);
-                
+
                 break;
             }
         }
-        
+
         return $result;
     }
-    
+
     public static function registerExtension(Extension $extension)
     {
         $this->extensions[] = $extension;
     }
-    
+
     public static function deregisterExtension(Extension $extension)
     {
-        foreach($this->extensions as $key => $posext)
-        {
+        foreach ($this->extensions as $key => $posext) {
             //If objects of same class
-            if($posext == $extension)
-            {
+            if ($posext == $extension) {
                 unset($this->extensions[$key]);
             }
         }
     }
-    
+
     public static function registerDefaultExtension(Extension $extension)
     {
         self::$defaults[] = $extension;
     }
-    
+
     public static function deregisterDefaultExtension(Extension $extension)
     {
-        foreach(self::$defaults as $key => $posext)
-        {
+        foreach (self::$defaults as $key => $posext) {
             //If objects of same class
-            if($posext == $extension)
-            {
+            if ($posext == $extension) {
                 unset(self::$defaults[$key]);
             }
         }
     }
-    
+
     public function init()
     {
         self::$init = true;
-        
+
         self::registerDefaultExtension(new Test());
         self::registerDefaultExtension(new Toolkit());
         self::registerDefaultExtension(new Formatter());
