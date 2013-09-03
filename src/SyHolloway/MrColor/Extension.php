0878a6895564865d58c2cc8c5f969089ea76d0d1
Simon Holloway <holloway.sy@gmail.com>
2013-09-03 22:39:49 +0100

    PSR-2'd it up

--- src/SyHolloway/MrColor/Extension.php
+++ src/SyHolloway/MrColor/Extension.php
@@ -4,22 +4,35 @@ namespace SyHolloway\MrColor;
 use SyHolloway\MrColor\Color;
 
 /**
- * Provides a parent class for MrColor extensions 
+ * Provides a parent class for MrColor extensions.
  *
  * @package MrColor
  * @author Simon Holloway
  */
 abstract class Extension
 {
+    /**
+     * Check if this object wants to handel this method call.
+     * 
+     * @param  string $method
+     * @return boolean
+     */
     public function query($method)
     {
         return (is_callable(array($this, $method)));
     }
     
+    /**
+     * Handel the method call.
+     * 
+     * @param  Color   $color   the color object the function was called on
+     * @param  string  $method  the method name
+     * @param  array   $args    arguments
+     * @return mixed            this will be returned to the client code
+     */
     public function trigger(Color $color, $method, $args)
     {
-        if(is_callable(array($this, $method)))
-        {
+        if (is_callable(array($this, $method))) {
             array_unshift($args, $color);
             
             return call_user_func_array(array($this, $method), $args);
